03f90ee3085bcbdd974b3ca55e32e30de8f55b77
AbnerThat <dylanabner14@gmail.com>
2023-02-20 00:37:13 -0600

    Adeed stripe webhooks 8

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -20,13 +20,11 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
     cancel_url: `${req.protocol}://${req.get('host')}/tour/${tour.slug}`,
     customer_email: req.user.email,
     client_reference_id: req.params.tourId,
-    mode: 'payment',
     line_items: [
       {
-        quantity: 1,
         price_data: {
           currency: 'usd',
-          unit_amount: tour.price * 100,
+          unit_amount: tour.price,
           product_data: {
             name: `${tour.name} Tour`,
             description: tour.summary,
@@ -36,9 +34,11 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
               }`
             ]
           }
-        }
+        },
+        quantity: 1
       }
-    ]
+    ],
+    mode: 'payment'
   });
 
   // 3) Create session as response
@@ -62,8 +62,9 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
 
 const createBookingCheckout = async session => {
   const tour = session.client_reference_id;
-  const user = await User.findOne({ email: session.customer_email }).id;
-  const price = session.amount_total / 100;
+  const user = (await User.findOne({ email: session.customer_email })).id;
+  const price = session.amount_total;
+  console.log(tour, user, price);
   await Booking.create({ tour, user, price });
 };
 
